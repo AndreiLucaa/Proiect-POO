1fd517dfdcbf5ed02445ffa6725c25ef7029bf58
AndreiLucaa <mihaiandreiluca16@yahoo.com>
2025-04-24 13:19:29 +0300

    changed include typo

--- Game.cpp
+++ Game.cpp
@@ -13,7 +13,7 @@
 #include <thread>
 #include "Country.h"
 #include "Globle.h"
-#include "json.hpp"
+#include "ext/include/json/json.hpp"
 #include "Wordle.h"
 
 Game::Game(Player &player): player(player) {
